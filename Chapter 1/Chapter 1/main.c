ec8370d4c3e2c044198ce55633046e6069b4b53c
Sergey Chernyaev <chernayev.sergey.workgmail.com>
2023-12-25 19:00:16 +0100

    Playinh around with output and program structure

---
+++ Chapter 1/Chapter 1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Chapter 1
+//
+//  Created by Sergey Chernyaev on 2023-12-25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
